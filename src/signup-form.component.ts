2f4163c474c39841b79be21b124b68c022c0de2e
adam.kulasiak <adam.kulasiak@gmail.com>
2019-07-30 08:12:05 +0200

    validation messages

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+    
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'signup-form',
@@ -6,26 +7,26 @@ import { Component, Output, EventEmitter } from '@angular/core';
     <form (submit)="onSubmit()" novalidate>
       <div class="form-group">
         <label>Email</label>
-        <input type="email" name="email" class="form-control" [(ngModel)]="email" required pattern=".+@.+">
+        <input type="email" class="form-control" [(ngModel)]="email" name="email" #emailField="ngModel"
+               required pattern=".+@.+">
+        <p *ngIf="emailField.touched && emailField.invalid && emailField.errors.required" 
+            class="alert alert-danger">Wymagany jest adres email</p>
+        <p *ngIf="emailField.touched && emailField.invalid && emailField.errors.pattern" 
+            class="alert alert-danger">Adres email jest nieprawidłowy</p>
       </div>
       <button type="submit" class="btn btn-primary">Rejestracja</button>
     </form>
   `,
-  styles: [`
-    input.ng-touched.ng-invalid {
-      border: solid red 2px;
-    }
-    input.ng-touched.ng-valid {
-      border: solid green 2px;
-    }
-  `]
+  styles:[`
+        input.ng-touched.ng-invalid { border:solid red 2px}
+        input.ng-touched.ng-valid { border:solid green 2px}
+    `]
 })
 export class SignupFormComponent {
 
   email = '';
 
-  onSubmit() {
-    console.log('dziala ', this.email);
+  onSubmit(){
+    console.log('Test onSubmit', this.email);
   }
-
 }
